9a380ed5fb14025eb9bc7bd4dbff847f5b132d48
Berk4823 <Berk4823@gmail.com>
2017-10-03 19:18:59 -0400

    Updating ui.R

--- Tanking3/ui.R
+++ Tanking3/ui.R
@@ -2,6 +2,9 @@ library(markdown)
 library(dplyr)
 library(ggplot2)
 library(data.table)
+# loading packages
+
+# I used the standard template for Shiny Dashboard
 
 dashboardPage(skin = "green",
   dashboardHeader(title = "Tanking?"),
@@ -13,8 +16,10 @@ dashboardPage(skin = "green",
         menuSubItem("Lottery Simulator", tabName = "lotterysim", icon = icon("play")),
         menuSubItem("Lottery Probabilities", tabName = "lotteryprob", icon = icon("th-large"))
       ),
+      
       menuItem("Player Data", tabName = "player", icon = )
     ),
+    
     conditionalPanel(
       condition = "input.menu1 == 'lotteryprob'",
       sliderInput("VarTeam", 
@@ -24,11 +29,13 @@ dashboardPage(skin = "green",
                   value = 1, 
                   step = 1)
     ),
+    
     conditionalPanel(
       condition = "input.menu1 == 'lotterysim'",
       h5("Simulate the Lottery"),
       actionButton("action", label = "Press Me!")
     ),
+    
     conditionalPanel(
       condition = "input.menu1 == 'player'",
       selectInput("player", 
@@ -36,10 +43,13 @@ dashboardPage(skin = "green",
                   choices = c("Number of Championships", "Win Shares", "All Star"),
                   selected = "Number of Championships", multiple = F)
     )
+    # there are three conditional panels above. These will only appear when certain conditions are met. I did this 
+    # in order to reduce the clutter and only keep widgets visible when they are needed. 
   ),
   dashboardBody(
     tags$head(
       tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+      
     ),
     tabItems(
       tabItem(tabName = "dashboard",
@@ -52,7 +62,9 @@ dashboardPage(skin = "green",
                 tags$li("Picks 4-14 are given by reverse order of standings to the teams remaining."),
                 tags$li("Link to full rules:", tags$a(href= "https://www.draftsite.com/nba/rules/", "Click here!"))
               )
+              # text on the main page
             ),
+            
             tabPanel("Proposed Lottery Rules",
               tags$ul(id = "Rules2",
                 tags$li("Split the non-playoff teams into two halves of 7, Bottom 7 and Top 7"), 
@@ -61,8 +73,10 @@ dashboardPage(skin = "green",
                 tags$li("Remaining picks 4-7 are given by reverse order of standings."),
                 tags$li("Repeat this process for Picks 8-14 with the Top 7 teams.")
               )
+              # text on the main page
             )
           ),
+          
           tabBox(width = 12, height = "250px", id = "tabset3",
             tabPanel("Current Lottery Issues",
               tags$ul(id = "Issues", 
@@ -70,46 +84,60 @@ dashboardPage(skin = "green",
                 tags$li("No strong alternative to the lottery for these teams improve."), 
                 tags$li("Once mathematically eliminated, borderline playoff teams", br(), "also have incentive to tank.")
               )
+              # text on the main page
             ),
+            
             tabPanel("Proposed Fixes",
               tags$ul(id = "Fixes1", 
                 tags$li("Decrease the bottom few teams' chance of winning the top picks."), 
                   tags$ul(id = "Fixes2",
                     tags$li("This should help dis-incentivize bottom teams from tanking.")
                   )
+                # text on the main page
               ),
+              
               tags$ul(id = "Fixes3", 
                 tags$li("Increase the borderline-playoff teams' chance of moving up in the draft."),
                   tags$ul(id = "Fixes4",
                     tags$li("This should incentivize borderline teams to finish out the season strong.")
                   )
               )
+              # text on the main page
             )
           )
         )
       ),
+      
       tabItem(tabName = "lotterysim",
         fluidRow(
-          infoBoxOutput("test1"),
-          infoBoxOutput("test2")
+          infoBoxOutput("winner_current"),
+          infoBoxOutput("winner_proposed")
+          # info box widget displaying the team that won each lottery. 
         ),
+        
         fluidRow(
-          valueBoxOutput("test3"), 
-          valueBoxOutput("test4")
+          valueBoxOutput("current_percent"), 
+          valueBoxOutput("proposed_percent")
+          # value box widget displaying the % chance for the winning team for each lottery. 
         ),
+        
         fluidRow(
           box(width = 6, background = "olive", collapsible = T,
             height = "auto",
             h3(id = "tabseta", "Current Lottery System"), 
-            tableOutput("value")
+            tableOutput("value_current_results")
+            # box displaying the full lottery results for the current system. 
           ),
+          
           box(width = 6,
             height = "auto", background = "blue", collapsible = T,
             h3(id = "tabsetb", "Proposed Lottery System"),
-            tableOutput("value2")
+            tableOutput("value_proposed_results")
+            # box displaying the full lottery results for the proposed system. 
           )
         )
       ),
+      
       tabItem(tabName = "player",
         fluidRow(
           box(width = 5,
@@ -122,6 +150,8 @@ dashboardPage(skin = "green",
                           choices = c("mean", "median", "min", "max"),
                           selected = "mean", multiple = F)
             ),
+            # selectInput widget to choose a stat for player data, inside of a conditional panel
+            
             conditionalPanel(
               condition = "input.player == 'Number of Championships'",
               selectInput("type_stat2", 
@@ -130,13 +160,15 @@ dashboardPage(skin = "green",
                           selected = "mean", multiple = F)
             )
           ),
+          # selectInput widget to choose a stat for championship data, inside of a conditional panel
           box(width = 12, 
               height = "auto",
               plotOutput("playerplot")
-            
           )
+          # output that returns a different plot depending on the choice made in the above widgets
         )
       ),
+      
       tabItem(tabName = "lotteryprob",
         fluidRow(
           tabBox(width = 12, id = "tabset7", title = uiOutput("team_title"), 
@@ -146,6 +178,8 @@ dashboardPage(skin = "green",
                  plotOutput("probability2")
                  )
           ),
+          # box showing the probability distribution in one tab, and probability differential in another tab. 
+          
           box(width = 12,
               height = "auto",
               tabBox(width = 12, id = "tabset6",
@@ -153,6 +187,7 @@ dashboardPage(skin = "green",
                      tabPanel("Proposed Lottery Probabilities", dataTableOutput("PLTable"))),
               collapsible = T, collapsed = T
           )
+          # box showing the probability tables for each lottery. 
         )
       )
     )
